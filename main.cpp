30ba5924a2aebfcbc3fe5c9249cdb7204aee6c8a
andrew9691 <andrew9691@yandex.ru>
2017-11-07 11:30:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,11 @@ using namespace std;
 int main()
 {
 	srand((unsigned)time(NULL));
-	barley_break s;
-	s.solvable_random_fill(70);
+	//barley_break s;
+	//s.solvable_random_fill(50);
+	//vector<vector<int>> m = { { 7, 5, 10, 11 },{ 2, 12, 4, 1 },{ 6, 13, 3, 8 },{ 9, 15, 0, 14 } }; check
+	vector<vector<int>> m = { { 1, 2, 3, 4 },{ 5, 6, 7, 8 },{ 9, 10, 11, 0 },{ 13, 14, 15, 12 } };
+	barley_break s = barley_break(m);
 	s.print();
 	cout << endl;
 	unsigned int start = clock();
